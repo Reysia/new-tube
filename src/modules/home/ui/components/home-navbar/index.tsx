b9c43c4d53215217ef54c026a9f71593e9dcb01d
LEE JAE MIN <kukiene@naver.com>
2025-11-23 18:53:24 +0900

    11/23 기본 레이아웃 구성

---
+++ src/modules/home/ui/components/home-navbar/index.tsx
@@ -0,0 +1,37 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import {SidebarTrigger} from "@/components/ui/sidebar";
+import {SearchInput} from "@/modules/home/ui/components/home-navbar/search-input";
+import {AuthButton} from "@/modules/auth/ui/components/auth-button";
+
+
+
+export const HomeNavbar = () => {
+    return (
+        <nav className={"fixed top-0 left-0 right-0 h-16 bg-white flex items-center px-2 pr-5 z-50"}>
+            <div className={"flex items-center gap-4 w-full"}>
+                {/* 메뉴와 로고 */}
+                <div className={"flex items-center flexshrink-0"}>
+                    <SidebarTrigger/>
+                    <Link href={"/"}>
+                        <div className={"p-4 flex items-center gap-1"}>
+                            <Image src={"logo.svg"} alt={"logo"} width={32} height={32}/>
+                            <p className={"text-xl font-semibold tracking-tight"}>
+                                NewTube
+                            </p>
+                        </div>
+                    </Link>
+                </div>
+                {/* 검색창 */}
+                <div className={"flex-1 flex justify-center max-w-[720px] mx-auto"}>
+                    <SearchInput/>
+                </div>
+                {/* 로그아웃 */}
+                <div className={"flex-shrink-0 items-center flex gap-4"}>
+                    <AuthButton />
+                </div>
+            </div>
+        </nav>
+    )
+}
